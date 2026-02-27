f2c1fafeb69d3daab746c9e8b64527f912664e48
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2026-02-27 11:13:10 -0500

    Add HookContext example

--- examples/hooks/fltest_hooks_template.py
+++ examples/hooks/fltest_hooks_template.py
@@ -1,5 +1,32 @@
 from fltest.core import hooks
 
+"""
+class HookContext:
+
+    cfg: Optional[Any] = None
+    round: Optional[int] = None
+    client_id: Optional[int] = None
+
+    # Data phase
+    raw_dataset: Optional[Any] = None
+    partition_map: Optional[Dict[int, Any]] = None
+    dist_dict: Optional[Dict[int, Any]] = None
+    test_data: Optional[Any] = None
+
+    # Model / state (Flower uses list of ndarrays for parameters)
+    client_data: Optional[Any] = None  # per-client training data/loader
+    global_state: Optional[Any] = None
+    client_update: Optional[Any] = None
+    num_samples: Optional[int] = None
+    updates_and_weights: Optional[List[tuple]] = None  # list of (update, n)
+
+    # Outputs
+    new_global_state: Optional[Any] = None
+    metrics: Optional[Dict[str, Any]] = None
+    history: Optional[Dict[str, Any]] = field(default_factory=dict)
+"""
+
+
 @hooks.before_simulation
 def log_before_simulation(ctx):
     pass
